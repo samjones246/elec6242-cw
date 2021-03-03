6e7631235f5730b5171a127e84484286a483fa7f
Sam Jones <sam.jones240699@gmail.com>
2021-03-03 15:06:33 +0000

    solved q2

--- q2/lsb.py
+++ q2/lsb.py
@@ -1,3 +1,4 @@
+import string
 def extract_lsbs(bytestring: bytearray):
     out = ""
     for b in bytestring:
@@ -7,6 +8,7 @@ def extract_lsbs(bytestring: bytearray):
 if __name__ == "__main__":
     with open("q2.hex", "rb") as f:
         bytestring = bytearray(f.read())
-        lsbs = extract_lsbs(bytestring)
+        lsbs = extract_lsbs(bytestring)[1::2]
+        print(lsbs)
         result = [lsbs[i:i+8] for i in range(0, len(lsbs), 8)]
-        print([chr(int(byte, base=2)) for byte in result])
+        print(" ".join([chr(int(byte, base=2)) for byte in result]))
--- q2/transform.py
+++ q2/transform.py
@@ -12,43 +12,79 @@ def decimal(bytestring : bytearray):
         out.append(f"{int(c):03d}")
     return out
 
+def asBinary(bytestring):
+    return " ".join([f"{int(bin(b)[2:]):08d}" for b in bytestring])
+
+def mergePairHalves(odds, evens):
+    out = []
+    for i in range(len(odds)):
+        byte1 = f"{int(bin(odds[i])[2:]):08d}"[:4]
+        byte2 = f"{int(bin(evens[i])[2:]):08d}"[4:]
+        byte = byte1 + byte2
+        out.append(chr(int(byte, base=2)))
+    return out
+
+def flipMSB_evens(odds, evens):
+    out = []
+    for i in range(len(odds)):
+        byte1 = f"{int(bin(odds[i])[2:]):08d}"
+        byte2 = "0" + f"{int(bin(evens[i])[2:]):08d}"[1:]
+        out.append(int(byte1, base=2))
+        out.append(int(byte2, base=2))
+    return out
+
+def arbitraryMapDistinctBytes(odds, evens):
+    distinctA = sorted(list(set(odds)))
+    distinctB = sorted(list(set(evens)))
+    mapA = {distinctA[i] : string.ascii_lowercase[i] for i in range(len(distinctA))}
+    mapB = {distinctB[i] : string.ascii_uppercase[i] for i in range(len(distinctB))}
+    useMap = mapA
+    translated = []
+    for b in bytestring:
+        translated.append(useMap[b])
+        useMap = mapB if useMap == mapA else mapA
+    return translated
+
+def getPairFrequencies(odds, evens):
+    pairs = [(odds[i], evens[i]) for i in range(len(odds))]
+    pairFrequencies = {}
+    for pair in pairs:
+        if pair not in pairFrequencies:
+            pairFrequencies[pair] = 0
+        pairFrequencies[pair] += 1
+
+def mirrorBytes(bytestring, onlyEvens=False):
+    out = []
+    even = False
+    for b in bytestring:
+        if even or (not onlyEvens):
+            out.append("".join(reversed(hex(b)[2:])))
+        else:
+            out.append(b)
+        even = not even
+    return out
+
+def xor(bytestring, key):
+    out = []
+    for b in bytestring:
+        out.append(b ^ key)
+    return "".join([chr(b) for b in out])
+
+def xor2(bytestring, key1, key2):
+    out = []
+    i = key1
+    for b in bytestring:
+        out.append(b ^ i)
+        if i == key1:
+            i = key2
+        else:
+            i = key1
+    return "".join([chr(b) for b in out])
+
 if __name__ == "__main__":
     with open("q2.hex", "rb") as f:
         bytestring = bytearray(f.read())
         odds = bytestring[::2]
         evens = bytestring[1::2]
-        print(f"{min(odds)}, {max(odds)}")
-        print(f"{min(evens)}, {max(evens)}")
-        print(" ".join([f"{int(bin(b)[2:]):08d}" for b in bytestring]))
-        something = []
-        for i in range(len(odds)):
-            byte1 = f"{int(bin(odds[i])[2:]):08d}"[:4]
-            byte2 = f"{int(bin(evens[i])[2:]):08d}"[4:]
-            byte = byte1 + byte2
-            something.append(chr(int(byte, base=2)))
-        something = []
-        for i in range(len(odds)):
-            byte1 = f"{int(bin(odds[i])[2:]):08d}"
-            byte2 = "0" + f"{int(bin(evens[i])[2:]):08d}"[1:]
-            something.append(int(byte1, base=2))
-            something.append(int(byte2, base=2))
-        distinctA = sorted(list(set(odds)))
-        distinctB = sorted(list(set(evens)))
-        print(len(distinctA))
-        print(len(distinctB))
-        mapA = {distinctA[i] : string.ascii_lowercase[i] for i in range(len(distinctA))}
-        mapB = {distinctB[i] : string.ascii_uppercase[i] for i in range(len(distinctB))}
-        useMap = mapA
-        translated = []
-        for b in bytestring:
-            translated.append(useMap[b])
-            useMap = mapB if useMap == mapA else mapA
-        print("".join(translated))
-        print("".join(translated[::2]))
-        print("".join(translated[1::2]))
-        pairs = [(odds[i], evens[i]) for i in range(len(odds))]
-        pairFrequencies = {}
-        for pair in pairs:
-            if pair not in pairFrequencies:
-                pairFrequencies[pair] = 0
-            pairFrequencies[pair] += 1
+        i = 244
+        print(xor2(bytestring, 110, i))
